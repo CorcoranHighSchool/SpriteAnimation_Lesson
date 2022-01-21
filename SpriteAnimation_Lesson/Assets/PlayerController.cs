59895fbb997c6570f2cd7374eb71731dd3a083cf
OzHache <OzHache@gmail.com>
2022-01-21 11:38:17 -0800

    Initial Sprite Load
    Still working on the Player Controller

---
+++ SpriteAnimation_Lesson/Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    //Speed of the character
+    private float m_speed = 5.0f;
+    private float m_jumpForce = 10.0f;
+    private bool isOnGround = true;
+    private Vector2 m_movement = Vector2.zero;
+
+    //Components
+    private Rigidbody2D m_rigidbody;
+    private Animator m_animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float jump = Input.GetAxis("Jump");
+        m_movement = new Vector2(horizontalInput, jump);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = Vector2.zero;
+        direction.x = Mathf.Clamp(m_movement.x * m_speed - m_rigidbody.velocity.x, -m_speed, m_speed);
+        bool jump = m_movement.y > 0.0f;
+
+        m_rigidbody.AddForce(direction);
+        if(jump && isOnGround)
+        {
+            m_rigidbody.AddForce(Vector2.up * m_jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = collision.gameObject.CompareTag("Floor");
+    }
+
+}
